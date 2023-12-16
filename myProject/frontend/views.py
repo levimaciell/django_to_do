b66226551d0057e4183fbf52385f353ea8cc8d79
levimaciell <levimaciell1221@gmail.com>
2023-12-16 02:06:07 -0300

    Listing tasks and creating tasks

--- myProject/frontend/views.py
+++ myProject/frontend/views.py
@@ -1,13 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import requests
 
 # Create your views here.
 
 def login(request):
-    return HttpResponse('This should be a login page')
+    pass
 
 def register(request):
     return HttpResponse('This should be a Register page')
 
 def todo(request):
-    return HttpResponse('This should be where todos are listed')
+
+    response = requests.get('http://127.0.0.1:8080/api/todo')
+
+    if response.status_code == 200:
+        tasks = response.json()
+    else:
+        tasks = 'Loading...'
+    
+    context = {'tasks': tasks}
+    return render(request, 'frontend/index.html', context)
